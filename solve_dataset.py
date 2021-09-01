2f38997f4c529fe79dc258161aec9f9a3110b5ef
snur717 <shahnursyed@gmail.com>
2021-08-31 21:11:52 -0400

    adding edge cases

--- solve_dataset.py
+++ solve_dataset.py
@@ -1,13 +1,17 @@
 from gen_dataset import *
+from board_setup import *
+import numpy as np
 
 #Using backtracking algo 
 def solve_puzzle(grid):
-    if solve_puzzle_helper(grid):   
-        print_grid(grid)
+    if check_duplicates(np.array(grid), np.array(grid).reshape(3, 3, -1, 3).swapaxes(1,2).reshape(-1, 3,3)):
+        raise ValueError("Invalid board -- contains duplicates")
+    elif np.count_nonzero(grid) < 17:
+        raise ValueError("Invalid board -- not enough provided clues")
+    elif solve_puzzle_helper(grid):   
         return grid
     else:
         raise ValueError("No solution")
-
 
 def solve_puzzle_helper(grid):
  #Base case, checking if we reached a solution
@@ -30,6 +34,30 @@ def solve_puzzle_helper(grid):
         #All combos were tried with a given num and no solution was reached
         return False       
 
-print("Final call")
-grid = fill_grid()
-solve_puzzle(grid)
+def check_duplicates(grid, squares):
+    for x in range(9):
+        row = np.unique(grid[x], return_counts = True)
+        col = np.unique(grid[:,x], return_counts = True)
+        sq = np.unique(squares[x], return_counts = True)
+
+        if row[0][0] == 0:
+            if (np.any(row[1][1:] > 1)):
+                return True
+        elif (np.any(row[1] > 1)):
+            return True
+
+        if col[0][0] == 0:
+            if (np.any(col[1][1:] > 1)):
+                return True
+        elif (np.any(col[1] > 1)):
+            return True
+        
+        if sq[0][0] == 0:
+            if (np.any(sq[1][1:] > 1)):
+                return True
+        elif (np.any(sq[1] > 1)):
+            return True
+
+    return False
+
+#grid.reshape(3, 3, -1, 3).swapaxes(1,2).reshape(-1, 3,3))
