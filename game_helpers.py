c6633b6ebce1862bab47ca3b71f55a37b41bcffe
snur717 <shahnursyed@gmail.com>
2021-09-18 01:11:20 -0400

    Update

--- game_helpers.py
+++ game_helpers.py
@@ -1,5 +1,55 @@
 import numpy as np
 import pygame as py
+import game_modes as modes
+import gen_dataset as gen_data
+
+
+def set_grid(easy, medium, hard, default):
+    """Generate a grid according to modes
+
+    Args:
+        easy (bool): easy mode selected
+        medium (bool): medium mode selected
+        hard (bool): hard mode selected
+        default (modes.Mode): randomly selected mode 
+    """
+
+    if easy:
+        grid = gen_data.gen_puzzle(0, 2, 4, 5, 42)
+    elif medium:
+        grid = gen_data.gen_puzzle(3, 5, 5, 6, 53)
+    elif hard:
+        grid = gen_data.gen_puzzle(6, 8, 6, 7, 64)
+    else:
+        if default == modes.Mode.EASY:
+            grid = gen_data.gen_puzzle(0, 2, 4, 5, 42)
+        elif default == modes.Mode.MEDIUM:
+            grid = gen_data.gen_puzzle(3, 5, 5, 6, 53)
+        else:
+            grid = gen_data.gen_puzzle(6, 8, 6, 7, 64)
+
+    return grid
+
+
+def set_ents_dicts(used_spots):
+    """Set to dictionarys to keep track of a Sudoku puzzle solution
+       and entries
+
+    Args:
+    sol (numpy.ndarray): solution of Sudoku puzzle
+    used_spots (list):  indices of cells with given clues
+    """
+
+    entries = {}
+
+    for idx in get_cell_indices():
+        if idx not in used_spots:
+            entries[idx] = 0
+        else:
+            entries[idx] = True
+
+    return entries
+
 
 def round_num(num):
     """Rounding number based on which range its falls in
@@ -52,29 +102,9 @@ def get_cell_indices():
     cell_indices = []
 
     for idx in np.ndindex(9, 9):
-        cell_indices.append(tuple(t *60 for t in idx))
+        cell_indices.append(tuple(t * 60 for t in idx))
 
     return cell_indices
-
-
-def set_ents_dicts(used_spots):
-    """Set to dictionarys to keep track of a Sudoku puzzle solution
-       and entries
-
-    Args:
-    sol (numpy.ndarray): solution of Sudoku puzzle
-    used_spots (list):  indices of cells with given clues
-    """
-
-    entries = {}
-
-    for idx in get_cell_indices():
-        if idx not in used_spots:
-            entries[idx] = 0
-        else:
-            entries[idx] = True
-
-    return entries
 
 
 def get_curr_grid_vals(entries, used_spots=None, grid=None):
@@ -84,6 +114,7 @@ def get_curr_grid_vals(entries, used_spots=None, grid=None):
     sol (numpy.ndarray): solution of Sudoku puzzle
     used_spots (list):  indices of cells with given clues
     """
+
     curr_grid_vals = np.asarray(list(entries.values())).reshape((9, 9))
     curr_grid_vals = np.transpose(curr_grid_vals)
 
@@ -97,73 +128,34 @@ def get_curr_grid_vals(entries, used_spots=None, grid=None):
 
 
 def get_square(curr_vals, x, y):
+    """Gets 3x3 square of a cell using (x, y) position
+
+    Args:
+    curr_vals (numpy.ndarray): current vals of Sudoku puzzle
+    x (int): x position of current cell 
+    y (int): y position of current cell
+    """
+
     sq_x_init = 3 * round(((x // 60) - 1) / 3)
     sq_y_init = 3 * round(((y // 60) - 1) / 3)
-    
+
     return curr_vals[sq_y_init:sq_y_init + 3, sq_x_init:sq_x_init + 3]
 
 
 def count_occurrences(curr_vals, num, x, y):
+    """Count occurrences of given num in row, col, and square
+
+    Args:
+    curr_vals (numpy.ndarray): current vals of Sudoku puzzle
+    num (int): number from 1 - 9
+    x (int): x position of current cell 
+    y (int): y position of current cell
+    """
+
     square = get_square(curr_vals, x, y)
 
-    col_count = np.count_nonzero(curr_vals[:,x // 60] == num)
-    row_count = np.count_nonzero(curr_vals[y // 60,:] == num)
+    col_count = np.count_nonzero(curr_vals[:, x // 60] == num)
+    row_count = np.count_nonzero(curr_vals[y // 60, :] == num)
     sq_count = np.count_nonzero(square == num)
 
     return (col_count, row_count, sq_count)
-
-
-def advance(screen, cell, grid, entries, invalid_ent=False):
-    num_font = py.font.SysFont('Monotype', 20, bold=True)
-    BLACK = (0, 0, 0)
-    BLUE = (0, 0, 255)
-    RED = (255, 0, 0)
-
-    (x, y, color) = cell
-    cell_idx = (x, y)
-    idx = (y // 60, x // 60)
-
-    py.draw.rect(screen, color, py.Rect(x + 3, y + 3, 56, 56)) 
-
-    
-    if str(entries[cell_idx]).isnumeric() and entries[cell_idx] != 0:
-        if invalid_ent:
-            screen.blit(num_font.render(str(entries[cell_idx]), True, RED),
-                (25 + x, 25 + y)
-            )
-        else:
-            screen.blit(num_font.render(str(entries[cell_idx]), True, BLUE),
-                (25 + x, 25 + y)
-            )
-    elif not str(entries[cell_idx]).isnumeric():
-        screen.blit(num_font.render(str(grid[idx]), True, BLACK),
-            (25 + x, 25 + y)
-        )
-
-def enter_board_adv(screen, cell, entries_ent, sol_ent=False):
-    (x1, y1, color) = cell
-    BLACK = (0, 0, 0)
-    BLUE = (0, 0, 255)
-    num_font = py.font.SysFont('Monotype', 20, bold=True)
-
-    #Erase previous empty square
-    py.draw.rect(screen, color, py.Rect(x1 + 3, y1 + 3, 56, 56)) 
-    
-    if type(sol_ent) == np.ndarray and entries_ent[(x1, y1)] == 0:
-        screen.blit(num_font.render(str(sol_ent[(y1//60, x1//60)]), True, BLUE),
-                (25 + x1, 25 + y1)
-        )
-    else:
-        if entries_ent[(x1, y1)] != 0:
-            screen.blit(num_font.render(str(entries_ent[(x1, y1)]), True, BLACK),
-                    (25 + x1, 25 + y1)
-            )
-
-
-# def set_font_color(invalid, assist, ):
-#     if (x, y) in invalid.keys() and assist:
-#         screen.blit(num_font.render(str(grid[(curr_y // 60, curr_x // 60)]), 
-#         True, BLACK), (25 + curr_x, 25 + curr_y))
-#     else:
-#         screen.blit(num_font.render(str(grid[(curr_y // 60, curr_x // 60)]), 
-#         True, BLACK), (25 + curr_x, 25 + curr_y))
